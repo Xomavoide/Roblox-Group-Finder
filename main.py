74ed783f9c5e966c883893ed8bc7ad1fbd4fd0b6
Xomavoide <100534221+Xomavoide@users.noreply.github.com>
2024-02-03 20:55:24 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,35 @@
-import os 
+import os
 import threading
-import requests, random
+import requests
+import random
 from dhooks import Webhook
-import ctypes
-ctypes.windll.kernel32.SetConsoleTitleW("Aleks Group Finder")
+import time
 
+time.sleep(5)
+
+webhook_url = 'https://discordapp.com/api/webhooks/1203460768591388672/dy8fDuicMI2kgCPyo-kBzB0-IyJ-mtkl_m5R8RNsL0cGLRgwtrSNxkGuyKZpOpyBMJ1h'
+hook = Webhook(webhook_url)
 
 def groupfinder():
-    id = random.randint(1000000, 1150000)
-    r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}") 
+    id = random.randint(0, 5007736)
+    r = requests.get(f"https://www.roblox.com/groups/group.aspx?gid={id}")
+
+    print(f"Checking group: {id}")
+
     if 'owned' not in r.text:
         re = requests.get(f"https://groups.roblox.com/v1/groups/{id}")
-        if 'isLocked' not in re.text and 'owner' in re.text:
-            if re.json()['publicEntryAllowed'] == True and re.json()['owner'] == None:
-                hook.send(f'Hit: https://www.roblox.com/groups/group.aspx?gid={id}')
-                print(f"[+] Hit: {id}")
+        if 'isLocked' not in re.text:
+            # Check if 'publicEntryAllowed' key is True in the response JSON
+            if 'publicEntryAllowed' in re.json() and re.json()['publicEntryAllowed'] == True:
+                hook.send(f'Found group: https://www.roblox.com/groups/group.aspx?gid={id}')
+                print(f"[+] Found group: {id}")
             else:
-                print(f"[-] No Entry Allowed: {id}")
+                print(f"[-] Group is Closed or No Public Entry Allowed: {id}")
         else:
-            print(f"[-] Group Locked: {id}")
+            print(f"[-] Group is Locked: {id}")
     else:
         print(f"[-] Group Already Owned: {id}")
 
-
-print("""
-
-
-____ _    ____ _  _ ____    ____ ____ ____ _  _ ___  
-|__| |    |___ |_/  [__     | __ |__/ |  | |  | |__] 
-|  | |___ |___ | \_ ___]    |__] |  \ |__| |__| |    
-                                                     
-____ _ _  _ ___  ____ ____ 
-|___ | |\ | |  \ |___ |__/ 
-|    | | \| |__/ |___ |  \ 
-                           
-""")
-
-#your webhook
-hook = input("[-] Enter your webhook url: "))
-#number of threads
 threads = int(input("[-] How many threads: "))
 
 while True:
